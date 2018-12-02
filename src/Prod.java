d981181d7a6d900cc06c0204d8bf1346105b1924
user <user@user-PC>
2018-12-02 09:16:01 +0530

    prod-5.0

---
+++ src/Prod.java
@@ -0,0 +1,15 @@
+
+public class Prod {
+	
+	private Integer pId;
+	
+	private String PName;
+
+	@Override
+	public String toString() {
+		return "Prod [pId=" + pId + ", PName=" + PName + "]";
+	}
+	
+	
+	
+}
